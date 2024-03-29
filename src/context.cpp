3cc7cedf883b345ca0e989a7064a33e37fcc2deb
Daniel <danielkalmucki2@gmail.com>
2024-03-29 19:02:33 +0100

    Split context to hpp and cpp

---
+++ src/context.cpp
@@ -0,0 +1,46 @@
+#include "context.hpp"
+
+#include <ncurses.h>
+
+std::shared_ptr<Context> Context::create(std::unique_ptr<State> state)
+{
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    refresh();
+
+    return std::make_shared<Context>(std::move(state), Private());
+}
+
+Context::~Context() { endwin(); }
+
+bool Context::render()
+{
+    if (state_ == nullptr)
+    {
+        return false;
+    }
+    state_->render();
+    return true;
+}
+
+void Context::changeState(std::unique_ptr<State> state)
+{
+    if (state == nullptr)
+    {
+        state_ = nullptr;
+        return;
+    }
+    state_ = std::move(state);
+    state_->setContext(shared_from_this());
+}
+
+void Context::setContextForState() { state_->setContext(shared_from_this()); }
+
+std::unordered_map<std::string, std::string> Context::getFormValues() { return form_values_; }
+
+void Context::setFormValues(const std::unordered_map<std::string, std::string>& form_values)
+{
+    form_values_ = form_values;
+}
--- src/context.hpp
+++ src/context.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <ncurses.h>
-
 #include <memory>
 #include <unordered_map>
 
@@ -14,41 +12,15 @@ class Context : public std::enable_shared_from_this<Context>
     };
 
   public:
-    static std::shared_ptr<Context> create(std::unique_ptr<State> state)
-    {
-        initscr();
-        cbreak();
-        noecho();
-        keypad(stdscr, TRUE);
-        refresh();
-
-        return std::make_shared<Context>(std::move(state), Private());
-    }
+    static std::shared_ptr<Context> create(std::unique_ptr<State> state);
+    ~Context();
     Context(std::unique_ptr<State> state, Private p) : state_(std::move(state)) {}
-    ~Context() { endwin(); }
 
-    bool render()
-    {
-        if (state_ == nullptr)
-        {
-            return false;
-        }
-        state_->render();
-        return true;
-    }
-    void changeState(std::unique_ptr<State> state)
-    {
-        if (state == nullptr)
-        {
-            state_ = nullptr;
-            return;
-        }
-        state_ = std::move(state);
-        state_->setContext(shared_from_this());
-    }
-    void setContextForState() { state_->setContext(shared_from_this()); }
-    std::unordered_map<std::string, std::string> getFormValues() { return form_values_; }
-    void setFormValues(const std::unordered_map<std::string, std::string>& form_values) { form_values_ = form_values; }
+    bool render();
+    void changeState(std::unique_ptr<State> state);
+    void setContextForState();
+    std::unordered_map<std::string, std::string> getFormValues();
+    void setFormValues(const std::unordered_map<std::string, std::string>& form_values);
 
   private:
     std::unique_ptr<State> state_;
