53a7699210c3ef6989bc765b1000de2ceccb4e60
Daniel <danielkalmucki2@gmail.com>
2024-05-10 18:17:58 +0200

    Multiple clients, fix clang format, adjust logger

--- src/host_state.cpp
+++ src/host_state.cpp
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <functional>
 #include <memory>
 #include <thread>
@@ -49,40 +50,58 @@ void HostState::render()
         exit(-1);
     }
 
-    listen(socketfd, 10);
-
-    int clientfd = accept(socketfd, 0, 0);
-
     const std::string &name = form_values.at("name");
-    send(clientfd, name.c_str(), name.size(), 0);
-
-    char client_name[25] = {0};
-    recv(clientfd, client_name, 25, 0);
+    std::vector<int> clients;
 
     int new_line_index = 0;
-    std::thread printer = std::thread(
-        [&clientfd, &chat_window, &new_line_index, &client_name]()
+    std::thread listener_thread = std::thread(
+        [&socketfd, &clients, &name, &chat_window, &new_line_index]
         {
             while (true)
             {
-                char buffer[256] = {0};
-                if (recv(clientfd, buffer, 255, 0) == 0)
-                {
-                    return 0;
-                }
-                mvwprintw(chat_window.get(), 1 + new_line_index, 1, "%s: %s", client_name, buffer);
-                new_line_index++;
-                box(chat_window.get(), 0, 0);
-                wrefresh(chat_window.get());
+                listen(socketfd, 10);
+
+                int clientfd = accept(socketfd, 0, 0);
+                clients.push_back(clientfd);
+                LOG_INFO() << "Accepted client: " << std::to_string(clientfd).c_str();
+
+                send(clientfd, name.c_str(), name.size(), 0);
+
+                char client_name[25] = {0};
+                recv(clientfd, client_name, 25, 0);
+
+                std::thread printer = std::thread(
+                    [&socketfd, &chat_window, &new_line_index, &clientfd, &client_name, &clients]()
+                    {
+                        while (true)
+                        {
+                            char buffer[256] = {0};
+                            if (recv(clientfd, buffer, 255, 0) == 0)
+                            {
+                                return 0;
+                            }
+
+                            std::for_each(clients.begin(), clients.end(),
+                                          [&buffer, &clientfd](const int &client)
+                                          {
+                                              if (client != clientfd) send(client, buffer, 255, 0);
+                                          });
+                            mvwprintw(chat_window.get(), 1 + new_line_index, 1, "%s: %s", client_name, buffer);
+                            new_line_index++;
+                            box(chat_window.get(), 0, 0);
+                            wrefresh(chat_window.get());
+                        }
+                    });
+                printer.detach();
             }
         });
-    printer.detach();
+    listener_thread.detach();
 
     while (true)
     {
         char buffer[256] = {0};
         getInput(buffer, input_window, chat_window, new_line_index, name);
-        send(clientfd, buffer, 255, 0);
+        std::for_each(clients.begin(), clients.end(), [&buffer](const int &client) { send(client, buffer, 255, 0); });
     }
 
     close(socketfd);
--- src/logger.hpp
+++ src/logger.hpp
@@ -1,34 +1,72 @@
 #pragma once
 
 #include <fstream>
-#include <memory>
 #include <string>
 
 const std::string LOG_DIR = "/home/mastermod1/Projects/ChattingRoom/";
 
-class Logger
+enum class LogLevel
 {
-  public:
-    virtual void setFunctionName(const std::string& name) = 0;
-    virtual void operator<<(const std::string& out) = 0;
-    virtual ~Logger() = default;
+    LOG_ERROR,
+    LOG_WARNING,
+    LOG_INFO
 };
 
-class FileLogger : public Logger
+class FileLogger
 {
   public:
-    FileLogger();
-    void setFunctionName(const std::string& name) override { name_ = name; }
-    void operator<<(const std::string& out) override;
-    ~FileLogger();
+    explicit FileLogger(const char* fname = "log.txt")
+    {
+        file_.open(fname);
+
+        if (file_.is_open())
+        {
+            file_ << "Log file created\n";
+        }
+    }
+
+    ~FileLogger()
+    {
+        if (file_.is_open())
+        {
+            file_.close();
+        }
+    }
+
+    friend FileLogger& operator<<(FileLogger& logger, const LogLevel level)
+    {
+        switch (level)
+        {
+            case LogLevel::LOG_ERROR:
+                logger.file_ << "[ERROR]: ";
+                break;
+
+            case LogLevel::LOG_WARNING:
+                logger.file_ << "[WARNING]: ";
+                break;
+
+            default:
+                logger.file_ << "[INFO]: ";
+                break;
+        }
+        return logger;
+    }
+
+    friend FileLogger& operator<<(FileLogger& logger, const char* text)
+    {
+        logger.file_ << text << std::endl;
+        return logger;
+    }
+
+    FileLogger(const FileLogger&) = delete;
+    FileLogger& operator=(const FileLogger&) = delete;
 
   private:
     std::ofstream file_;
-    std::string name_ = "";
 };
 
-const static std::unique_ptr<Logger> LOGGER = std::make_unique<FileLogger>();
+static FileLogger logger = FileLogger();
 
-#define LOG_INFO()                                \
-    LOGGER->setFunctionName(__PRETTY_FUNCTION__); \
-    *LOGGER
+#define LOG_INFO() logger << LogLevel::LOG_INFO << " " << __PRETTY_FUNCTION__
+
+// LOGGER->setFunctionName(__PRETTY_FUNCTION__);
